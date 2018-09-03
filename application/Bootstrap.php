4abe243c03b6e1de134035943f4d552237fbeb75
Biuni <info.gianlucabonifazi@gmail.com>
2018-09-03 10:13:10 +0200

    Update initial release

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,60 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	protected $_logger;
+	protected $_view;
 
+	/**
+	* Initalize the layout metadata
+	* and the CSS / Javascript files
+	*/
+	protected function _initViewSettings()
+    {
+        $this->bootstrap('view');
+        $this->_view = $this->getResource('view');
+        $this->_view->headMeta()->setCharset('UTF-8');
+        $this->_view->headMeta()->appendName('viewport', 'width=device-width,initial-scale=1,shrink-to-fit=no');
+
+        $this->_view->headTitle('Health Facility | Gianluca Bonifazi - Progetto Tecnologie Web');
+
+        $this->_view->headLink()->appendStylesheet('css/bootstrap.min.css');
+        $this->_view->headLink()->appendStylesheet('css/style.css');
+
+        $this->_view->InlineScript()->appendFile('js/jquery-3.3.1.slim.min.js');
+        $this->_view->InlineScript()->appendFile('js/popper.min.js');
+        $this->_view->InlineScript()->appendFile('js/bootstrap.min.js');
+    }
+
+	/**
+	* Initalize the logger
+	*/
+    protected function _initLogging()
+    {
+        $writer = new Zend_Log_Writer_Stream(APPLICATION_PATH . '/data/log/logger.log');        
+        $logger = new Zend_Log($writer);
+
+        Zend_Registry::set('log', $logger);
+
+        $this->_logger = $logger;
+    	$this->_logger->info('Bootstrap ' . __METHOD__);
+    }
+
+	/**
+	* Initalize database connection
+	*/
+	protected function _initDbParms()
+    {
+		require_once(APPLICATION_PATH . '/configs/global.php');
+
+		$db = new Zend_Db_Adapter_Pdo_Mysql(array(
+    		'host'     => DB_HOST,
+    		'username' => DB_USER,
+    		'password' => DB_PWD,
+    		'dbname'   => DB_NAME
+		));  
+		
+		Zend_Db_Table_Abstract::setDefaultAdapter($db);
+	}
 
 }
 
---
+++ application/configs/global.php
@@ -0,0 +1,13 @@
+<?php 
+return [
+	
+	/**
+	 * Database connection config
+	 */
+	define('DB_HOST', 'localhost'),
+	define('DB_NAME', 'health'),
+	define('DB_USER', 'root'),
+	define('DB_PWD', '')
+	
+];
+?>
